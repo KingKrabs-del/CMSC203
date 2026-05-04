148b34e24289e123183d056ddb6cf85575e94728
KingKrabs-del <ishaking53@gmail.com>
2026-05-04 19:09:11 -0400

    Update PropertyDriver.java

--- CMSC203_Assignment4/PropertyDriver.java
+++ CMSC203_Assignment4/PropertyDriver.java
@@ -2,18 +2,18 @@
  * Instructor: Dr. Grinberg
  * Due: 3/30/2026
  * Platform/compiler: Java
+ * Description: The program lets the user add properties, view a list of all properties, and calculate the total rent collected from all properties.
  * I pledge that I have completed the programming assignment independently.
- * I have not copied the code from a student or any source. I have not given
- * my code to any student.
+ * I have not copied the code from a student or any source. I have not given my code to any student.
  * Ishatta King
  */
 public class PropertyDriver {
     public static void main(String[] args) {
-        ManagementCompany mc = new ManagementCompany("Campus Realty", "123-45-6789");
+        ManagementCompany mc = new ManagementCompany("Six-Seven Street", "123-45-6789");
 
-        Property p1 = new Property("Apt 1", "Rockville", 1200.0, "Bob");
-        Property p2 = new Property("Apt 2", "Rockville", 1500.0, "Carol");
-        Property p3 = new Property("Apt 3", "Silver Spring", 1800.0, "David");
+        Property p1 = new Property("Apt 67", "Silver Spring", 1200.0, "JohnPork");
+        Property p2 = new Property("Apt 41", "Silver Spring", 1500.0, "Tungahur");
+        Property p3 = new Property("Apt 01", "Silver Spring", 1800.0, "TimCheese");
 
         mc.addProperty(p1);
         mc.addProperty(p2);
