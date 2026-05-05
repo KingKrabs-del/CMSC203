dd1123bc481eb891b93b1292e102252914cbdd3b
KingKrabs-del <ishaking53@gmail.com>
2026-05-04 21:49:29 -0400

    Create Lion.java

---
+++ CMSC203_Assignment6/Lion.java
@@ -0,0 +1,58 @@
+public class Lion implements Animal {
+    private String name;
+    private int age;
+    private String species;
+    private String color;
+    private String imagePath;
+
+    public Lion(String name, int age, String species, String color, String imagePath) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+        this.color = color;
+        this.imagePath = imagePath;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " roar");
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + " runs");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    @Override
+    public String toString() {
+        return "Lion --- Name: " + name + ", Age: " + age + ", Species: " + species +", Color: " + color;
+    }
+
+
+}
+
