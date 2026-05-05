a4edbc82dc92b9eb097cb97a75f227f707645e99
KingKrabs-del <ishaking53@gmail.com>
2026-05-04 21:47:16 -0400

    Create CustomInputMismatchException.java

---
+++ CMSC203_Assignment6/CustomInputMismatchException.java
@@ -0,0 +1,9 @@
+
+
+import java.util.InputMismatchException;
+
+public class CustomInputMismatchException extends InputMismatchException {
+ public CustomInputMismatchException(String message) {
+     super(message);
+ }
+}
